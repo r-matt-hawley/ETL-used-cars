f80c3250656425eaedf1fa54bdfcb8dbfbe7e7a1
clarkdavis6 <clarkdavis6@gmail.com>
2019-02-26 20:40:32 -0600

    additions to latest TT py file

--- AutoApp.py
+++ AutoApp.py
@@ -12,13 +12,13 @@ def scrape():
     browser = init_browser()
     listings = {}
 
-    url = "https://www.truecar.com/used-cars-for-sale/listings/year-2017-max/location-houston-tx/?searchRadius=100&sort[]=price_desc"
+    url = "https://www.truecar.com/used-cars-for-sale/listing/ZHWUR1ZF1JLA10082/2018-lamborghini-huracan/"
     browser.visit(url)
 
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
-    listings["headline"] = soup.find("a", class_="result-title").get_text()
+    listings["location"] = soup.find("span", attrs={"data-qa":"used-vdp-header-location"}).text
     listings["price"] = soup.find("span", class_="result-price").get_text()
     listings["hood"] = soup.find("span", class_="result-hood").get_text()
 
