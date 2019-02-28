c7480e7426910dd1582674fc9bdb4683dd674851
Matt Hawley <r.matt.hawley@gmail.com>
2019-02-27 23:08:26 -0600

    update TruCarScraper.py

--- TruCarScraper.py
+++ TruCarScraper.py
@@ -2,10 +2,8 @@ import requests
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 
-!which chromedriver
-
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
+# executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+# browser = Browser('chrome', **executable_path, headless=False)
 
 # https://www.cars.com/vehicledetail/detail/762102222/overview/
     
@@ -13,26 +11,25 @@ url = "https://www.truecar.com/used-cars-for-sale/listings/year-2017-max/locatio
 response = requests.get(url)
 soup = bs(response.text, 'html.parser')
 
-scraped = {}
+
 
 boxInfoList = soup.find_all('div', attrs={"data-qa": "VehicleDetailsSm"})#.find_all("div",attrs={"data-qa":True})
 
+# while
+scraped = {}
 for box in boxInfoList:
     boxInfo = box.find_all("div",attrs={"data-qa":True})
 
-# Get color ingo
+# Get color info
     for rez in boxInfo: 
         scraped[rez["data-qa"].split("-")[2]]=rez.text
         
 
-        vin = soup.find_all("a", attrs={"data-qa": "VehicleListing"})[0]["href"].split("/")[3]
-    print(vin)
+    vin = soup.find_all("a", attrs={"data-qa": "VehicleListing"})[0]["href"].split("/")[3]
     scraped["VIN"] = vin
 
-    results = soup.find('a', attrs={"data-qa": "VehicleListing"})
-
-    AutoListing = results.find_all("h4")
-
+    # Year, Make, Model, and Price are all in the same a tag.
+    AutoListing = soup.find('a', attrs={"data-qa": "VehicleListing"}).find_all("h4")
     header = AutoListing[0].text.split(" ")
     scraped["Year"] = header[0]
     scraped["Make"] = header[1]
